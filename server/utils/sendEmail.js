93ce5a2f475e7f8eaec133bf351cfb27778ed1dd
islam1645 <saidoun.islam@gmail.com>
2025-12-26 23:12:18 +0100

    brevo smtp

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,25 +2,26 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // <--- ON UTILISE LE PRÉRÉGLAGE INTERNE
+    host: process.env.EMAIL_HOST, // Sera smtp-relay.brevo.com
+    port: Number(process.env.EMAIL_PORT), // Sera 587
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    // On garde ça pour éviter les problèmes de certificats
     tls: {
       rejectUnauthorized: false
     }
   });
 
   const message = {
-    from: `KutubDZ <${process.env.EMAIL_USER}>`,
+    from: `KutubDZ <${process.env.EMAIL_USER}>`, // L'email que tu as validé sur Brevo
     to: options.email,
     subject: options.subject,
     text: options.message,
   };
 
-  console.log("Tentative envoi via Service Gmail...");
+  console.log("Tentative envoi SMTP...");
   await transporter.sendMail(message);
   console.log("Email envoyé !");
 };
